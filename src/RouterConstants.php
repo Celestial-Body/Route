848899953f435bcb7aea63a7e1213623eb1bf432
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-28 16:04:52 -0400

    Update RouterConstants.php

--- src/RouterConstants.php
+++ src/RouterConstants.php
@@ -1,1 +1,34 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Route.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Genial-Framework/Route> Github repository.
+ * @license <https://github.com/Genial-Framework/Route/blob/master/LICENSE> BSD 3-Clause.
+ */
 
+namespace Genial\Route;
+
+/**
+ * RouterConstants.
+ */
+class RouterConstants
+{
+
+    /**
+     * @var array $methods The list of all methods.
+     */
+    const METHODS = [
+        'GET',
+        'POST',
+        'PATCH',
+        'DELETE',
+        'PUT',
+        'HEAD',
+        'OPTIONS',
+        'CONNECT'
+    ];
+
+}
