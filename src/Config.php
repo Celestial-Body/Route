ff0a2bc99d4ad7453c9f60fbb7ebbbfbd5a61f60
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-27 22:31:15 -0400

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -1,1 +1,10 @@
-
+<?php
+declare(strict_types=1);
+/**
+ * Genial Route.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <> Github repository.
+ * @license </blob/master/LICENSE> .
+ */
