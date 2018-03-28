3661b0281deb29538137278bf558c2d397a511b5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-28 10:22:15 -0400

    Update and rename Config.php to Collection.php

---
+++ src/Collection.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Route.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Genial-Framework/Route> Github repository.
+ * @license <https://github.com/Genial-Framework/Route/blob/master/LICENSE> BSD 3-Clause.
+ */
+
+namespace Genial\Route;
+
+use ArrayAccess;
+use SplObjectStorage;
+
+/**
+ * Collection.
+ */
+class Collection implements CollectionInterface, ArrayAccess
+{ 
+}
