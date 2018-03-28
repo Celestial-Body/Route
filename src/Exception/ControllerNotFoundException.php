76fc3e388385e7a4750e096ef33c3fd7ba62d247
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-28 17:44:07 -0400

    Update ControllerNotFoundException.php

--- src/Exception/ControllerNotFoundException.php
+++ src/Exception/ControllerNotFoundException.php
@@ -1,1 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Route.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Genial-Framework/Route> Github repository.
+ * @license <https://github.com/Genial-Framework/Route/blob/master/LICENSE> BSD 3-Clause.
+ */
 
+namespace Genial\Route\Exception;
+
+/**
+ * ControllerNotFoundException.
+ */
+class ControllerNotFoundException extends RouteException implements ExceptionInterface
+{
+}
