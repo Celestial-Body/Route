543e4c5343e24565478d42a4c423839d433ba14b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-28 17:41:27 -0400

    Update RouteException.php

--- src/Exception/RouteException.php
+++ src/Exception/RouteException.php
@@ -1,1 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Route.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Genial-Framework/Route> Github repository.
+ * @license <https://github.com/Genial-Framework/Route/blob/master/LICENSE> BSD 3-Clause.
+ */
 
+namespace Genial\Route;
+
+use Exception;
+
+/**
+ * RouteException.
+ */
+class RouteException extends Exception implements ExceptionInterface
+{
+}
