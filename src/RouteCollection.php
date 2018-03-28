0d2c68c95cfb622173c0a36ea32aff1ebfcc9464
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-28 17:57:48 -0400

    Update RouteCollection.php

--- src/RouteCollection.php
+++ src/RouteCollection.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @link    <https://github.com/Genial-Framework/Route> Github repository.
  * @license <https://github.com/Genial-Framework/Route/blob/master/LICENSE> BSD 3-Clause.
  */
-
+ 
 namespace Genial\Route;
 
 use ArrayAccess;
@@ -18,4 +18,22 @@ use ArrayAccess;
  */
 class RouteCollection implements RouteCollectionInterface, ArrayAccess
 {
+    
+    /**
+     * @var array $routes A list of routes.
+     */
+    private $routes = [];
+    
+    /**
+     * Check to see a route exists.
+     *
+     * @param string $offset An offset to check for.
+     *
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->routes[$offset]);
+    }
+    
 }
