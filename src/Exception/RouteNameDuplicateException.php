6dcb9d12843b2b1166a8e4df9df5eaa842f4fddb
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-28 17:49:43 -0400

    Update RouteNameDuplicateException.php

--- src/Exception/RouteNameDuplicateException.php
+++ src/Exception/RouteNameDuplicateException.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 namespace Genial\Route\Exception;
 
 /**
- * RouteException.
+ * RouteNameDuplicateException.
  */
 class RouteNameDuplicateException extends RouteException implements ExceptionInterface
 {
