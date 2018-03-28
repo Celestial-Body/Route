86a36cba7b7dd9ec293e97a6d4de0196c9f80dae
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-27 23:11:27 -0400

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -19,4 +19,56 @@ use Symfony\Component\Yaml\Exception\ParseException;
  */
 class Config implements ConfigInterface
 {
+    
+    /**
+     * @var array $routeOptions The list of avaliable route options.
+     */
+    private $routeOptions = [
+        'path',
+        'controller'
+    ];
+    
+    /**
+     * Inject the route configuration with the route configuration instance.
+     *
+     * @param mixed $configuration The route configuration file or text.
+     *
+     * @throws Exception\InvalidRouteConfiguration If the route configuration is invalid.
+     *
+     * @return void Return nothing.
+     */
+    public function __construct($configuration)
+    {
+        if (\is_array($configuration)) {
+            if (Utils::arrayDepth($configuration) != 2) {
+                throw new Exception\InvalidRouteConfiguration(\sprintf(
+                    'The route configuration has an invalid array depth. Depth Passed: `%s`.',
+                    \strval(Utils::arrayDepth($configuration))
+                ));
+            }
+            foreach ($configuration as $key => $element) {
+                if (!\is_array($element)) {
+                    throw new Exception\InvalidRouteConfiguration(\sprintf(
+                        'The route configuration has an element with a depth of 1. Element Passed: `%s`.',
+                        \htmlspecialchars($key, \ENT_QUOTES)
+                    ));
+                }
+                foreach ($element as $routeOption => $value) {
+                    if (!\in_array($routeOption, $this->routeOptions) {
+                        throw new Exception\InvalidRouteConfiguration(\sprintf(
+                            'The route configuration has a route option that is unknown or unsupported. Route Option Passed: `%s`.',
+                            \htmlspecialchars($routeOption, \ENT_QUOTES)
+                        ));
+                    }
+                    if (!\is_string($value)) {
+                        throw new Exception\InvalidRouteConfiguration(\sprintf(
+                            'The route configuration has a route option value that is not a string. Type Passed: `%s`.',
+                            \gettype($value)
+                        ));
+                    }
+                }
+            }
+        }
+    }
+    
 }
